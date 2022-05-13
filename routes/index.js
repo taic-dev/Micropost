1738c72ab3a7407fe57601f4a6b37cd7683138d3
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-05-14 02:49:37 +0900

    メッセージ POST受け取り

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const messageArray = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Micropost' });
+  res.render('index', { 
+    title: 'Micropost',
+    messageArray: ''
+  });
+});
+
+router.post('/', function(req, res, next) {
+  const text = req.body['text'];
+  messageArray.push(text);
+
+  res.render('index', {
+    title: 'Micropost',
+    messageArray: messageArray
+  });
 });
 
 module.exports = router;
