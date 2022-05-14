2d4461da797c36371e8201f7ffebf77c6a08439b
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-05-14 09:16:30 +0900

    時間,ユーザー名 POST

---
+++ public/javascripts/index.js
@@ -0,0 +1,3 @@
+const getUsername = document.querySelector('#username').innerHTML;
+const addValue = document.getElementById('input-username');
+addValue.value = getUsername;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const router = express.Router();
+require('date-utils');
 const messageArray = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Micropost',
+    username: 'テストタロウ',
     messageArray: ''
   });
 });
 
 router.post('/', function(req, res, next) {
+  const username = req.body['username'];
   const text = req.body['text'];
-  messageArray.push(text);
+  const date = new Date();
+  const currentTime = date.toFormat('YYYY/MM/DD HH24:MI:SS')
+  messageArray.push([text,currentTime]);
+  console.log(messageArray);
 
   res.render('index', {
     title: 'Micropost',
+    username: username,
     messageArray: messageArray
   });
 });
