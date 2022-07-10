452021323758441224a318f59a2e742319231d92
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-07-10 19:24:25 +0900

    画像アップロード バリデーションの追加

--- controllers/validationList.js
+++ controllers/validationList.js
@@ -12,6 +12,16 @@ const validationList = {
             if(req.body.password == "" || req.body.password_confirmation == ""){throw new Error('パスワードを記入してください');}
             if(req.body.password !== req.body.password_confirmation){throw new Error('パスワードが一致していません');}
             return true;
+        }),
+        check('file').custom((value, {req})=>{
+            if(typeof req.file !== 'undefined'){
+                const extension = req.file.originalname.split('.').pop();
+                const imgSize = req.file.size;
+                const extensionsArray = ["png","jpg","jpeg"];
+                if(extensionsArray.indexOf(extension) == -1 ){throw new Error('画像はpngかjpg形式を使用してください')}
+                if(imgSize >= 1000000){throw new Error('1MB以下のサイズを使用してください')}
+            }
+            return true;
         })
     ],
     reset: [
